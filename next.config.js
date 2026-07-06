bd9fdb1472b103103e35eb6bc9a15c3b375f0c6e
Baron-eight <info@bigmasterz.com>
2026-07-06 04:25:26 -0700

    Reduce Next.js build memory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@deriv/core'],
+  experimental: {
+    webpackBuildWorker: false,
+    cpus: 1,
+  },
 }
 
 module.exports = nextConfig
